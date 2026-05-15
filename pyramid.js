1771fd1107c20acd19a1da2789956a5575b13a4d
dkolovou <dkolovou@gmail.com>
2026-05-15 15:16:23 +0300

    pyramid: implement pyramid shape builder

---
+++ pyramid.js
@@ -0,0 +1,8 @@
+const pyramid = (char, height) => {
+  let result = ''
+  for (let i = 1; i <= height; i++) {
+    if (i > 1) result += '\n'
+    result += ' '.repeat(height - i) + char.repeat(2 * i - 1)
+  }
+  return result
+}
