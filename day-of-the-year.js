d164e5c797393a4097867750d1ff2f707491e492
dkolovou <dkolovou@gmail.com>
2026-05-19 19:59:22 +0300

    day-of-the-year: calculate days since Jan 1 of the same year

---
+++ day-of-the-year.js
@@ -0,0 +1,6 @@
+const dayOfTheYear = (date) => {
+  const jan1 = new Date(0)
+  jan1.setFullYear(date.getFullYear(), 0, 1)
+  jan1.setHours(0, 0, 0, 0)
+  return Math.round((date - jan1) / 86400000)
+}
