5c5f454885061d56484d5e0312cfe6e21e316235
dkolovou <dkolovou@gmail.com>
2026-05-19 19:59:59 +0300

    day-of-the-year: use 1-based day counting

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -2,5 +2,5 @@ const dayOfTheYear = (date) => {
   const jan1 = new Date(0)
   jan1.setFullYear(date.getFullYear(), 0, 1)
   jan1.setHours(0, 0, 0, 0)
-  return Math.round((date - jan1) / 86400000)
+  return Math.round((date - jan1) / 86400000) + 1
 }
