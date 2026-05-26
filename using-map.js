9aaf688ce374bff9a4e7a19c614415578b40636d
dkolovou <dkolovou@gmail.com>
2026-05-26 20:09:01 +0300

    using-map: implement city/state/temperature mapping functions

---
+++ using-map.js
@@ -0,0 +1,17 @@
+const citiesOnly = (arr) => arr.map(({ city }) => city)
+
+const upperCasingStates = (arr) =>
+  arr.map(s => s.replace(/\b\w/g, c => c.toUpperCase()))
+
+const fahrenheitToCelsius = (arr) =>
+  arr.map(s => `${Math.floor((parseFloat(s) - 32) * 5 / 9)}°C`)
+
+const trimTemp = (arr) =>
+  arr.map(({ city, temperature }) => ({ city, temperature: temperature.replace(/\s/g, '') }))
+
+const tempForecasts = (arr) =>
+  arr.map(({ city, temperature, state }) => {
+    const c = Math.floor((parseFloat(temperature) - 32) * 5 / 9)
+    const capitalState = upperCasingStates([state])[0]
+    return `${c}°Celsius in ${city}, ${capitalState}`
+  })
